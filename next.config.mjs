1ba63252cca591d50008832fbeae347fac44de8b
PK <tonguip.mkl@gmail.com>
2025-02-03 17:16:15 +0700

    Update Next.js configuration to include experimental output settings and improve ESLint handling

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
 import createNextIntlPlugin from 'next-intl/plugin';
-
 const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         domains: ['cosi.bu.ac.th', 'github.com', 'www.countryflags.com'],
     },
     eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
+        ignoreDuringBuilds: true, // Allows build to complete even if ESLint errors exist
+    },
+    experimental: {
+        outputStandalone: true, // Ensures correct output for static exports
     },
 };
 

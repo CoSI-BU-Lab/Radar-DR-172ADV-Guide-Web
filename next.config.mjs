e5ecb765193d75328ffcbececd62f27b1113e34e
PK <tonguip.mkl@gmail.com>
2025-02-03 17:21:49 +0700

    Update Next.js configuration to set output mode and adjust image handling for GitHub Pages compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,19 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
     images: {
         domains: ['cosi.bu.ac.th', 'github.com', 'www.countryflags.com'],
     },
     eslint: {
         ignoreDuringBuilds: true, // Allows build to complete even if ESLint errors exist
     },
-    experimental: {
-        outputStandalone: true, // Ensures correct output for static exports
+    images: {
+        unoptimized: true, // Required since GitHub Pages doesn't support Next.js image optimization
     },
+    // experimental: {
+    //     outputStandalone: true, // Ensures correct output for static exports
+    // },
 };
 
 export default withNextIntl(nextConfig);

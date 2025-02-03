176d46a0c12a6a86894411490a8dd5aedafc7f61
PK <tonguip.mkl@gmail.com>
2025-02-03 17:27:09 +0700

    Refactor Next.js configuration to comment out distribution and output settings; retain image optimization settings for GitHub Pages compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 import createNextIntlPlugin from 'next-intl/plugin';
+
 const withNextIntl = createNextIntlPlugin();
+
 const nextConfig = {
-    distDir: "out",
-    output: "export",
+    // distDir: "out",
+    // output: "export",
     images: {
         domains: ['cosi.bu.ac.th', 'github.com', 'www.countryflags.com'],
+        unoptimized: true, // Required since GitHub Pages doesn't support Next.js image optimization
     },
     eslint: {
         ignoreDuringBuilds: true, // Allows build to complete even if ESLint errors exist
-    },
-    images: {
-        unoptimized: true, // Required since GitHub Pages doesn't support Next.js image optimization
     },
     // experimental: {
     //     outputStandalone: true, // Ensures correct output for static exports

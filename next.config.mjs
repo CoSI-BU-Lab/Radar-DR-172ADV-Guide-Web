d4918584270a4f91c7afd100052352b38a60030a
PK <tonguip.mkl@gmail.com>
2025-02-03 17:34:09 +0700

    Refactor GitHub Actions workflow and Next.js configuration for improved compatibility with GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,18 @@
 import createNextIntlPlugin from 'next-intl/plugin';
-
 const withNextIntl = createNextIntlPlugin();
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    // distDir: "out",
-    // output: "export",
-    // images: {
-    //     domains: ['cosi.bu.ac.th', 'github.com', 'www.countryflags.com'],
-    //     unoptimized: true, // Required since GitHub Pages doesn't support Next.js image optimization
-    // },
-    // eslint: {
-    //     ignoreDuringBuilds: true, // Allows build to complete even if ESLint errors exist
-    // },
+    distDir: "out",
+    output: "export",
+    images: {
+        domains: ['cosi.bu.ac.th', 'github.com', 'www.countryflags.com'],
+    },
+    eslint: {
+        ignoreDuringBuilds: true, // Allows build to complete even if ESLint errors exist
+    },
+    images: {
+        unoptimized: true, // Required since GitHub Pages doesn't support Next.js image optimization
+    },
     // experimental: {
     //     outputStandalone: true, // Ensures correct output for static exports
     // },
